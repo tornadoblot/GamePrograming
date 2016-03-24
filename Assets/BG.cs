aba3b028ead15443fc58ca20f5772fbcaf130391
tornadoblot <newclearplant@gmail.com>
2016-03-24 16:06:35 +0900

    asdf

---
+++ Assets/BG.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BG : MonoBehaviour {
+
+    public float ScrollSp = 1.5f;
+    Material asdf;
+
+	// Use this for initialization
+	void Start () {
+        asdf = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = asdf.mainTextureOffset.x + ScrollSp * Time.deltaTime;
+        Vector2 offset = new Vector2(x, 0);
+        asdf.mainTextureOffset = offset;
+	
+	}
+}
